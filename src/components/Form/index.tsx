14d8371cc4f1251694175037340e2e177bb3e998
Renan Souza <renantrsouza@gmail.com>
2022-03-04 13:15:43 -0400

    Passa os valores para uma função que está na tag pai

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,33 +1,32 @@
 import {TextField, Button, Switch, FormControlLabel} from '@mui/material'
 import {useState, ChangeEvent} from 'react'
-export const Form = () => {
+
+type FormProps = {
+    dataChange:(dataState: object) => void; //Tipagem da prop que recebe uma função que recebe um objeto e retorna void
+}
+export const Form = ({dataChange}: FormProps) => {
     const [name, setName] = useState<string>('')
     const [lastname, setLastname] = useState<string>('')
     const [cpf, setCpf] = useState<string>('')
+    const [promocoes, setPromocoes] = useState<boolean>(true)
+    const [novidades, setNovidades] = useState<boolean>(true)
 
     const handlerSubmit = (event: ChangeEvent<HTMLFormElement>) => { //Eventos do formulário
         event.preventDefault()
-        console.log('teste')
+        const dataState = {name, lastname, cpf, promocoes, novidades}
+        dataChange(dataState)
     }
     
     return (
         <form onSubmit={handlerSubmit}>
             <TextField id='name' value={name} onChange={(event)=>setName(event.target.value)} label='Nome' variant='outlined' margin="normal" fullWidth/>
 
-            <TextField 
-                id='lastname' 
-                value={lastname}
-                onChange={(event)=>setLastname(event.target.value)}
-                label='Sobrenome' 
-                variant='outlined' 
-                margin="normal" 
-                fullWidth
-            />
+            <TextField id='lastname' value={lastname} onChange={(event)=>setLastname(event.target.value)}label='Sobrenome' variant='outlined' margin="normal" fullWidth/>
             
             <TextField id='cpf' value={cpf} onChange={(event)=>setCpf(event.target.value)} label='CPF' variant='outlined' margin="normal" fullWidth/>
 
-            <FormControlLabel label="Promoções" control={<Switch name="Promoções" defaultChecked />} />
-            <FormControlLabel label="Novidades" control={<Switch name="Novidades" defaultChecked />} />
+            <FormControlLabel label="Promoções" control={<Switch name="Promoções" checked={promocoes} onChange={(event)=>setPromocoes(event.target.checked)} />} />
+            <FormControlLabel label="Novidades" control={<Switch name="Novidades" checked={novidades} onChange={(event)=>setNovidades(event.target.checked)} />} />
             
 
             <Button variant='contained' type="submit">Cadastrar</Button>
