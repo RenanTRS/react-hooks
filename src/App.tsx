fd79a292af065150ea9879aba40d7d9fa75e1b36
Renan Souza <renantrsouza@gmail.com>
2022-03-04 13:16:12 -0400

    tipagem e função quer receberá o objeto

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,21 @@ import { Form } from './components/Form'
 import '@fontsource/roboto/'
 function App() {
 
+  type dataFormProps = {
+    name?: string;
+    lastname?: string;
+    cpf?: string;
+    promocoes?: boolean;
+    novidades?: boolean;
+  }
+  
+  const dataForm = (props: dataFormProps) => {
+    console.log(props)
+  }
   return (
     <Container component="article" maxWidth="sm">
       <Typography variant="h3" component="h1" align="center">Formulário de cadastro</Typography>
-      <Form />
+      <Form dataChange={dataForm}/>
     </Container>
   )
 }
