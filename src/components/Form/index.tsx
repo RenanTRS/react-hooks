01dfb1da4c588fc98994cbd64557a4ab7f50c2f5
Renan Souza <renantrsouza@gmail.com>
2022-03-04 12:08:59 -0400

    uso do useState

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,12 +1,30 @@
 import {TextField, Button, Switch, FormControlLabel} from '@mui/material'
+import {useState, ChangeEvent} from 'react'
 export const Form = () => {
-    return (
-        <form>
-            <TextField id='name' label='Nome' variant='outlined' margin="normal" fullWidth/>
+    const [name, setName] = useState<string>('')
+    const [lastname, setLastname] = useState<string>('')
+    const [cpf, setCpf] = useState<string>('')
 
-            <TextField id='lastname' label='Sobrenome' variant='outlined' margin="normal" fullWidth/>
+    const handlerSubmit = (event: ChangeEvent<HTMLFormElement>) => { //Eventos do formulário
+        event.preventDefault()
+        console.log('teste')
+    }
+    
+    return (
+        <form onSubmit={handlerSubmit}>
+            <TextField id='name' value={name} onChange={(event)=>setName(event.target.value)} label='Nome' variant='outlined' margin="normal" fullWidth/>
+
+            <TextField 
+                id='lastname' 
+                value={lastname}
+                onChange={(event)=>setLastname(event.target.value)}
+                label='Sobrenome' 
+                variant='outlined' 
+                margin="normal" 
+                fullWidth
+            />
             
-            <TextField id='cpf' label='CPF' variant='outlined' margin="normal" fullWidth/>
+            <TextField id='cpf' value={cpf} onChange={(event)=>setCpf(event.target.value)} label='CPF' variant='outlined' margin="normal" fullWidth/>
 
             <FormControlLabel label="Promoções" control={<Switch name="Promoções" defaultChecked />} />
             <FormControlLabel label="Novidades" control={<Switch name="Novidades" defaultChecked />} />
