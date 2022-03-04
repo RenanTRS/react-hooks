94971883bc187571cd09f60b59021ff085c989a2
Renan Souza <renantrsouza@gmail.com>
2022-03-04 18:46:26 -0400

    Elevei o nível da função e a coloquei na mesma tipagem

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,18 @@ function App() {
   const dataForm = (props: dataFormProps) => {
     console.log(props)
   }
+  const validarCPF = (cpf: string) => {
+    if(cpf?.length !== 11){
+        return {valido: false, texto: "CPF deve ter 11 dígitos"}
+    } else {
+        return {valido: true, texto: ''}
+    }
+}
 
   return (
     <Container component="article" maxWidth="sm">
       <Typography variant="h3" component="h1" align="center">Formulário de cadastro</Typography>
-      <Form dataChange={dataForm} />
+      <Form dataChange={dataForm} validarCPF={validarCPF}/>
     </Container>
   )
 }
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,12 +3,10 @@ import {useState, ChangeEvent} from 'react'
 
 type FormProps = {
     dataChange:(dataState: object) => void; //Tipagem da prop que recebe uma função que recebe um objeto e retorna void
+    validarCPF: (cpf: string) => {valido:boolean; texto:string;}; //Tipagem para uma função que irá receber um valor e retornar um objeto com dois valores
 }
-/*
-type ValidarProps = {
-    validarCPF: (cpf: string) => {valido:boolean; texto:string;}
-}*/
-export const Form = ({dataChange}: FormProps) => {
+
+export const Form = ({dataChange, validarCPF}: FormProps) => {
     const [name, setName] = useState<string>('')
     const [lastname, setLastname] = useState<string>('')
     const [cpf, setCpf] = useState<string>('')
@@ -20,13 +18,6 @@ export const Form = ({dataChange}: FormProps) => {
         event.preventDefault()
         const dataState = {name, lastname, cpf, promocoes, novidades}
         dataChange(dataState)
-    }
-    const validarCPF = (cpf: string) => {
-        if(cpf?.length !== 11){
-            return {valido: false, texto: "CPF deve ter 11 dígitos"}
-        } else {
-            return {valido: true, texto: ''}
-        }
     }
     
     return (
@@ -44,7 +35,6 @@ export const Form = ({dataChange}: FormProps) => {
             <FormControlLabel label="Promoções" control={<Switch name="Promoções" checked={promocoes} onChange={(event)=>setPromocoes(event.target.checked)} />} />
             <FormControlLabel label="Novidades" control={<Switch name="Novidades" checked={novidades} onChange={(event)=>setNovidades(event.target.checked)} />} />
             
-
             <Button variant='contained' type="submit">Cadastrar</Button>
         </form>
     )
